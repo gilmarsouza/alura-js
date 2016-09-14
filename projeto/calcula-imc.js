38b0f9e45a87c75b663d5ef9d37a541a5bd03168
gilmarsouza <gilmar.limasou@gmail.com>
2016-09-13 22:18:26 -0300

    Capítulo 4: Coleções e Loops

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,15 +1,24 @@
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
+var trPacientes = document.getElementsByClassName("paciente");
 
-var paciente = { peso: tdPeso.textContent, altura: tdAltura.textContent };
+for (var posicaoAtual = 0; posicaoAtual <= trPacientes.length - 1; posicaoAtual++){
+    
+    var pacienteTr = trPacientes[posicaoAtual];
+    
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    
+    var paciente = { nome: tdNome.textContent, peso: tdPeso.textContent, altura: tdAltura.textContent };
+    
+    if (paciente.altura != 0) {
+        var imc = paciente.peso / (paciente.altura * paciente.altura);
 
-if (paciente.altura != 0) {
-    var imc = paciente.peso / (paciente.altura * paciente.altura);
-    
-    var tdImc = document.getElementById("imc-2");
-    tdImc.textContent = imc;
-    
-    console.log(imc);
-} else {
-    console.log("Não posso executar uma divisão por 0!");
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+        tdImc.textContent = imc;
+
+        console.log(imc);
+    } else {
+        console.log("Não posso executar uma divisão por 0!");
+    }    
 }
+
