c522c43b2afcd72d2399a16caf95916094200e52
gilmarsouza <gilmar.limasou@gmail.com>
2016-09-27 21:26:48 -0300

    Capítulo 6: Eventos

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,35 +1,39 @@
-var trsPacientes = document.getElementsByClassName("paciente");
+var botao = document.getElementById("calcula-imcs");
 
-percorreArray(trsPacientes, imprimeEModificaTdDeImc);
+botao.addEventListener("click", function(){
+    var trsPacientes = document.getElementsByClassName("paciente");
 
-function imprimeEModificaTdDeImc(pacienteTr){
-    var pacienteAtual = montaPaciente(pacienteTr);
-    var imc = pacienteAtual.pegaImc();
+    percorreArray(trsPacientes, imprimeEModificaTdDeImc);
+
+    function imprimeEModificaTdDeImc(pacienteTr){
+        var pacienteAtual = montaPaciente(pacienteTr);
+        var imc = pacienteAtual.pegaImc();
+
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+        tdImc.textContent = imc;
+
+        console.log(imc);
+    } 
     
-    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-    tdImc.textContent = imc;
-    
-    console.log(imc);
-}
+    function montaPaciente(pacienteTr){
+        var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+        var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+        var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-function montaPaciente(pacienteTr){
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
-
-    var paciente = { 
-        nome: tdNome.textContent, 
-        peso: tdPeso.textContent, 
-        altura: tdAltura.textContent,
-        pegaImc: function (){
-            if (this.altura != 0) {
-                var imc = this.peso / (this.altura * this.altura);
-                return imc;
-            } else {
-                console.log("Não posso executar uma divisão por 0!")
+        var paciente = { 
+            nome: tdNome.textContent, 
+            peso: tdPeso.textContent, 
+            altura: tdAltura.textContent,
+            pegaImc: function (){
+                if (this.altura != 0) {
+                    var imc = this.peso / (this.altura * this.altura);
+                    return imc;
+                } else {
+                    console.log("Não posso executar uma divisão por 0!")
+                }
             }
-        }
-    };
-    
-    return paciente;
-}
+        };
+
+        return paciente;
+    }  
+});
